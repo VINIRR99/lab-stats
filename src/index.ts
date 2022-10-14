c0715dd5f0877df89ebcf67a8e4fada4943835e5
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-10-14 10:15:06 -0400

    analyze football.csv

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
-console.log('Hello World!');
-console.log('Bye World!');
+import fs from "fs";
+
+const matches = fs.readFileSync('football.csv', { encoding: 'utf-8' })
+    .split('\n')
+        .map((match: string): string[] => match.split(','));
+
+let manUnitedWins = 0;
+const homeWin = 'H';
+const awayWin = 'A';
+const draw = 'D';
+
+for (let match of matches) {
+    if (
+        ((match[1] === 'Man United') && (match[5] === homeWin)) ||
+        ((match[2] === 'Man United') && (match[5] === awayWin))
+    ) manUnitedWins += 1;
+};
+
+console.log(`Man United won ${manUnitedWins} games`);
